0d0bea2b57a1406259477700274c1cdc4224be86
heoserin <sheo@hist.co.kr>
2022-12-05 13:35:28 +0900

    add: add annotation for component scan

--- src/main/java/com/example/xraytest/XraytestApplication.java
+++ src/main/java/com/example/xraytest/XraytestApplication.java
@@ -2,8 +2,10 @@ package com.example.xraytest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
+@ServletComponentScan
 @EnableAspectJAutoProxy
 @SpringBootApplication
 public class XraytestApplication {
